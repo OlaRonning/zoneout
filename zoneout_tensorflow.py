c9b6981573549e3f57840d8b849c9424dee8cf54
OlaRonning <vdl761@alumni.ku.dk>
2017-10-21 21:48:29 +0200

    fixed zoneout to comply with 1.3 api

--- zoneout_tensorflow.py
+++ zoneout_tensorflow.py
@@ -9,10 +9,11 @@ z_prob_states = 0
 # Wrapper for the TF RNN cell
 # For an LSTM, the 'cell' is a tuple containing state and cell
 # We use TF's dropout to implement zoneout
+import tensorflow as tf
 class ZoneoutWrapper(tf.nn.rnn_cell.RNNCell):
   """Operator adding zoneout to all states (states+cells) of the given cell."""
 
-  def __init__(self, cell, state_zoneout_prob, is_training=True, seed=None):
+  def __init__(self, cell, zoneout_prob, is_training=True, seed=None):
     if not isinstance(cell, tf.nn.rnn_cell.RNNCell):
       raise TypeError("The parameter cell is not an RNNCell.")
     if (isinstance(zoneout_prob, float) and
@@ -40,12 +41,12 @@ class ZoneoutWrapper(tf.nn.rnn_cell.RNNCell):
     output, new_state = self._cell(inputs, state, scope)
     if isinstance(self.state_size, tuple):
       if self.is_training:
-          new_state = tuple((1 - state_part_zoneout_prob) * tf.python.nn_ops.dropout(
+          new_state = tf.contrib.rnn.LSTMStateTuple(*((1 - state_part_zoneout_prob) * tf.python.nn_ops.dropout(
                         new_state_part - state_part, (1 - state_part_zoneout_prob), seed=self._seed) + state_part
-                            for new_state_part, state_part, state_part_zoneout_prob in zip(new_state, state, self._zoneout_prob))
+                            for new_state_part, state_part, state_part_zoneout_prob in zip(new_state, state, self._zoneout_prob)))
       else:
-          new_state = tuple(state_part_zoneout_prob * state_part + (1 - state_part_zoneout_prob) * new_state_part
-                            for new_state_part, state_part, state_part_zoneout_prob in zip(new_state, state, self._zoneout_prob))
+          new_state = tf.contrib.rnn.LSTMStateTuple(*(state_part_zoneout_prob * state_part + (1 - state_part_zoneout_prob) * new_state_part
+                            for new_state_part, state_part, state_part_zoneout_prob in zip(new_state, state, self._zoneout_prob)))
     else:
       if self.is_training:
           new_state = (1 - state_part_zoneout_prob) * tf.python.nn_ops.dropout(
@@ -56,5 +57,5 @@ class ZoneoutWrapper(tf.nn.rnn_cell.RNNCell):
 
 
 # Wrap your cells like this
-cell = ZoneoutWrapper(tf.nn.rnn_cell.LSTMCell(hidden_units, initializer=random_uniform(), state_is_tuple=True),
+cell = ZoneoutWrapper(tf.nn.rnn_cell.LSTMCell(hidden_units),
                                        zoneout_prob=(z_prob_cells, z_prob_states))
